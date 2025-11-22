5df90dd2cf1f69c26e3f705177ed00682104595e
sksoundari-web <sksoundari@gmail.com>
2025-11-22 12:08:00 +0530

    Updated code

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://travel-back-n00i.onrender.com/api"
+
+    
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
